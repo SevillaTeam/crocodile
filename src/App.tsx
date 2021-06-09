2cfe83b20e98ac2611495049b41fa36ec0abb61a
Mike <mik.arbuzov@gmail.com>
2021-06-09 19:20:27 +0300

    sass loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 
-import {Router} from '@components/router';
+import { Router } from '@components/router';
 import ErrorBoundary from '@components/ErrorBoundary';
 
 const App = () => (
-    <BrowserRouter>
-        {/* надо обернуть в Layout когда он появится */}
-        <ErrorBoundary>
-            <Router/>
-        </ErrorBoundary>
-    </BrowserRouter>
+  <BrowserRouter>
+    {/* надо обернуть в Layout когда он появится */}
+    <ErrorBoundary>
+      <Router />
+    </ErrorBoundary>
+  </BrowserRouter>
 );
 
-
-export {App};
+export { App };
