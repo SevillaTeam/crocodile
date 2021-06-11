c2f824208ebccea57563a7e6d4f9cc146aa4cf9e
Alexander <gituser1056@gmail.com>
2021-06-11 21:58:40 +0300

    Input согласно комментариям Review

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import heading from "./app.module.scss";
-import React from "react";
+import heading from './app.module.scss';
+import React from 'react';
+
+import { Input, IInputState } from './components/Input';
 
 interface Prop {
-    num: number
+  num: number;
 }
 
 const App = (prop: Prop) => (
-    <h1 className={heading.heading}>Crocodile With React and TypeScript! {prop.num}</h1>
+  <div>
+    <h1 className={heading.heading}>
+      Crocodile With React and TypeScript! {prop.num}
+    </h1>
+    <Input
+      placeholder='Placeholder 1'
+      type='text'
+      name='lal'
+      helpMessage='helpMessageText'
+      isError={false}
+      onChange={({ value, name }: IInputState) =>
+        console.log('name= value=', name, value)
+      }
+    />
+    <p></p>
+    <Input
+      placeholder='Placeholder 2'
+      type='text'
+      name='lal2'
+      onChange={({ value, name }: IInputState) =>
+        console.log('name= value=', name, value)
+      }
+      helpMessage='Some error occured'
+      isError={true}
+    />
+  </div>
 );
 
-export {App}
+export { App };
---
+++ src/components/Input/input.tsx
@@ -0,0 +1,74 @@
+import React, { PureComponent, ChangeEvent } from 'react';
+import { OwnInputProps, IInputState } from './';
+import cn from 'classnames';
+import s from './input.module.scss';
+
+type Props = OwnInputProps;
+
+export class Input extends PureComponent<Props, IInputState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { value: '', name: '' };
+  }
+
+  handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { onChange } = this.props;
+    this.setState({ value: e.target.value, name: e.target.name }, () => {
+      onChange(this.state);
+    });
+  };
+
+  handleBlur = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { onBlur } = this.props;
+    if (onBlur) {
+      this.setState({ value: e.target.value, name: e.target.name }, () => {
+        onBlur(this.state);
+      });
+    }
+  };
+
+  public render(): JSX.Element | React.ReactNode {
+    const {
+      isError,
+      helpMessage,
+      placeholder,
+      isRequired = false,
+      ...props
+    } = this.props;
+
+    return (
+      <div className={s.input}>
+        <label className={s.label}>
+          <input
+            className={cn([s.htmlinput], {
+              [s.htmlinput_error]: isError,
+            })}
+            {...props}
+            required={isRequired}
+            value={this.state.value}
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
+          />
+          {placeholder && (
+            <span
+              className={cn([s.placeholder], {
+                [s.placeholder_top]: this.state.value,
+              })}
+            >
+              {placeholder}
+            </span>
+          )}
+        </label>
+        {helpMessage && (
+          <span
+            className={cn([s.helpMessage], {
+              [s.helpMessage_error]: isError,
+            })}
+          >
+            {helpMessage}
+          </span>
+        )}
+      </div>
+    );
+  }
+}
