bebd75c6bc7161dfcf77253817dfbdf902a4855d
artbuzzon <62429835+artbuzzon@users.noreply.github.com>
2021-06-12 14:20:06 +0300

    initial config added (#1)
    * initial config added
    
    * cosmetics
    
    * cosmetics
    
    * sass loader added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import heading from "./app.module.scss";
+import React from "react";
+
+interface Prop {
+    num: number
+}
+
+const App = (prop: Prop) => (
+    <h1 className={heading.heading}>Crocodile With React and TypeScript! {prop.num}</h1>
+);
+
+export {App}
