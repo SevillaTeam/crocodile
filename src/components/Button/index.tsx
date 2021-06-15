40f02c964a09e2d199d428d2cc051fdd001c7bb9
Mike <mik.arbuzov@gmail.com>
2021-06-15 09:29:29 +0300

    types moved to a separate file

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import s from "./button.module.scss"
-import {ButtonProps} from "@components/Button/types";
+import React from 'react';
+import s from './button.module.scss';
+import { ButtonProps } from '@components/Button/types';
 
 const Button: React.FC<ButtonProps> = ({
-                                           onClick,
-                                           type = 'text',
-                                           size = 'standard',
-                                           color = 'primary',
-                                           disabled = false,
-                                           text
-                                       }) => (
-    <button disabled={disabled}
-            className={`${s.button} ${s[`button__${type}`]} ${s[`button__${size}`]} ${s[`button__${color}`]} ${s[`button__${disabled ? 'disabled' : ''}`]}`}
-            onClick={onClick}>{text}</button>
-)
+  onClick,
+  type = 'text',
+  size = 'standard',
+  color = 'primary',
+  disabled = false,
+  children,
+}) => (
+  <button
+    disabled={disabled}
+    className={`${s.button} ${s[`button__${type}`]} ${s[`button__${size}`]} ${
+      s[`button__${color}`]
+    } ${s[`button__${disabled ? 'disabled' : ''}`]}`}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
 
-export {Button}
+export { Button };
