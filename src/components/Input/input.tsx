33a6bc99d92deb7c1bdc4702680a85856971a192
Alexander <gituser1056@gmail.com>
2021-06-16 17:06:25 +0300

    Добавил type

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -6,7 +6,14 @@ import s from './input.module.scss';
 type Props = OwnInputProps;
 
 export const Input: FC<Props> = (props) => {
-  const { isError, helpMessage, name, placeholder, isRequired = false } = props;
+  const {
+    isError,
+    helpMessage,
+    name,
+    type = 'text',
+    placeholder,
+    isRequired = false,
+  } = props;
   const [inputValue, setInputValue] = useState({ value: '', name: '' });
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
@@ -30,6 +37,7 @@ export const Input: FC<Props> = (props) => {
           className={cn([s.htmlinput], {
             [s.htmlinput_error]: isError,
           })}
+          type={type}
           name={name}
           required={isRequired}
           value={inputValue.value}
