c40325a128c3d9f0955384dac1d5fec30050d7c5
Alexander <64581128+alex1056@users.noreply.github.com>
2021-06-19 18:41:42 +0300

    S5T17: Базовый компонент input (#8)
    * nvmrc added
    
    * sass loader added
    
    * Донастройка конфигов + input
    
    * Input c обновлениями
    
    * Input согласно комментариям Review
    
    * Input переделан на FC
    
    * Добавил type
    
    * Fix после pull
    
    Co-authored-by: Mike <mik.arbuzov@gmail.com>

---
+++ src/components/Input/input.tsx
@@ -0,0 +1,68 @@
+import React, { ChangeEvent, FC, useState } from 'react';
+import { OwnInputProps } from './';
+import cn from 'classnames';
+import s from './input.module.scss';
+
+type Props = OwnInputProps;
+
+export const Input: FC<Props> = (props) => {
+  const {
+    isError,
+    helpMessage,
+    name,
+    type = 'text',
+    placeholder,
+    isRequired = false,
+  } = props;
+  const [inputValue, setInputValue] = useState({ value: '', name: '' });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { onChange } = props;
+    setInputValue({ value: e.target.value, name: e.target.name });
+    onChange({ value: e.target.value, name: e.target.name });
+  };
+
+  const handleBlur = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { onBlur } = props;
+    if (onBlur) {
+      setInputValue({ value: e.target.value, name: e.target.name });
+      onBlur({ value: e.target.value, name: e.target.name });
+    }
+  };
+
+  return (
+    <div className={s.input}>
+      <label className={s.label}>
+        <input
+          className={cn([s.htmlinput], {
+            [s.htmlinput_error]: isError,
+          })}
+          type={type}
+          name={name}
+          required={isRequired}
+          value={inputValue.value}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {placeholder && (
+          <span
+            className={cn([s.placeholder], {
+              [s.placeholder_top]: inputValue.value,
+            })}
+          >
+            {placeholder}
+          </span>
+        )}
+      </label>
+      {helpMessage && (
+        <span
+          className={cn([s.helpMessage], {
+            [s.helpMessage_error]: isError,
+          })}
+        >
+          {helpMessage}
+        </span>
+      )}
+    </div>
+  );
+};
---
+++ src/components/Input/types.ts
@@ -0,0 +1,19 @@
+export type OwnInputProps = {
+  onChange: ({ value, name }: IInputState) => void;
+  onBlur?: ({ value, name }: IInputState) => void;
+  type?: 'text' | 'email' | 'password';
+  name: string;
+  id?: string;
+  placeholder?: string;
+  value?: string;
+  classNames?: Array<string>;
+  helpMessage?: string;
+  isError?: boolean;
+  isRequired?: boolean;
+  [propName: string]: unknown;
+};
+
+export interface IInputState {
+  value: string;
+  name: string;
+}
