660adfec10127dcea4cfc9a1d207e8f617afe6e1
Mike <mik.arbuzov@gmail.com>
2021-06-26 12:07:32 +0300

    waiting for users component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
-
-import { Layout } from './components/Layout';
+import { Layout } from '@components/Layout';
 import { Router } from '@components/router';
 import ErrorBoundary from '@components/ErrorBoundary';
 
