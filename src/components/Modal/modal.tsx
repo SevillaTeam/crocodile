cb4e1340520f14c71e4a669fa173fead9fc7d7b5
artbuzzon <62429835+artbuzzon@users.noreply.github.com>
2021-06-26 12:18:01 +0300

    S5T22: Модальное окно (#12)
    * modal started
    
    * modal done
    
    * cosmetics
    
    * cosmetics

---
+++ src/components/Modal/modal.tsx
@@ -0,0 +1,23 @@
+import React, {FC} from 'react';
+import {IModalState} from "@components/Modal/interfaces";
+import s from './modal.module.scss'
+
+export const Modal: FC<IModalState> = (props) => {
+
+    const onClose = () => {
+        props.onClose(false);
+    }
+
+    if (!props.isModalOpen) {
+        return null
+    } else {
+        return (
+            <div>
+                <div className={s.modal}>
+                    <div>{props.children}</div>
+                </div>
+                <div className={s.modalWrapper} onClick={onClose}></div>
+            </div>
+        )
+    }
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import s from './home.module.scss';
+import {Modal} from "@components/Modal";
+import {Button} from "@components/Button";
+import {IHomeState} from "@/pages/home/interfaces";
+
 
 export const Home = (): JSX.Element => {
-  return (
-    <div className={s.homePage}>
-      <h1 className={s.homePage__title}>Home page Croco</h1>
-      <p className={s.homePage__description}>Здесь все будет... скоро</p>
-      <Link className={s.homePage__link} to='/page-not-found'>
-        Перейти на 404 страницу
-      </Link>
-      <Link className={s.homePage__link} to='/game'>
-        Go to game page
-      </Link>
-    </div>
-  );
+
+    const [homeState, setHomeState] = useState<IHomeState>({
+        isModalOpen: false,
+    })
+
+
+    const showModal = () => {
+        setHomeState((homeState) => ({isModalOpen: !homeState.isModalOpen}));
+    }
+
+    const closeModal = (isOpened: boolean) => {
+        setHomeState((homeState) => ({isModalOpen: isOpened}));
+    }
+
+
+    return (
+        <div className={s.homePage}>
+            <h1 className={s.homePage__title}>Home page Croco</h1>
+            <p className={s.homePage__description}>Здесь все будет... скоро</p>
+            <Link className={s.homePage__link} to='/page-not-found'>
+                Перейти на 404 страницу
+            </Link>
+            <Link className={s.homePage__link} to='/game'>
+                Go to game page
+            </Link>
+            <Button text="Show modal" onClick={showModal}/>
+            <Modal onClose={closeModal} isModalOpen={homeState.isModalOpen}>
+                easy breezy
+            </Modal>
+        </div>
+    );
 };
