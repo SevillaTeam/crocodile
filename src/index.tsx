0ec952f5833797bcdd32b682c03b47fe7e0a7c9b
Alexander <gituser1056@gmail.com>
2021-07-07 14:15:50 +0300

    Убрал лишнее

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,8 @@ import './styles/_fonts.scss';
 import './styles/_global.scss';
 
 import { configureStore } from './store';
-import { changeUserId } from './store/user/actions';
 
 const store = configureStore({});
-
-// console.log(store.getState());
-// console.log(store.getState().user);
-
-// store.dispatch(changeUserId({ id: 0 }));
-// console.log(store.getState().user);
-
-// store.dispatch(changeUserId({ id: 1 }));
-// console.log(store.getState().user);
 
 ReactDOM.render(
   <React.StrictMode>
