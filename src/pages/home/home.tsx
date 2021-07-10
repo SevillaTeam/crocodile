866c9ff962c06d9fd1067425fcdc46d6d1e01efb
Mike <mik.arbuzov@gmail.com>
2021-07-10 15:24:21 +0300

    cosmetics

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import s from './home.module.scss';
 import * as Pages from '../../pages';
-import {WaitingForUsers} from "@components/WaitingForUsers/waiting-for-users";
-import { Link } from 'react-router-dom';
 
 export const Home = (): JSX.Element => {
   return (
-    <div className={s.homePage}>
-      <Pages.Login />
-      <h1 className={s.homePage__title}>Home page Croco</h1>
-      <p className={s.homePage__description}>Здесь все будет... скоро</p>
-      <Link className={s.homePage__link} to='/page-not-found'>
-        Перейти на 404 страницу
-      </Link>
-      <Link className={s.homePage__link} to='/game'>
-        Go to game page
-      </Link>
-        <WaitingForUsers visible/>
-    </div>
+      <div className={s.homePage}>
+          <Pages.Login />
+      </div>
   );
 };
