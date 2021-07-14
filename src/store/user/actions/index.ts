001eb9d7848a3776f9cc09b39c92ee2232c3ce65
Alexander <gituser1056@gmail.com>
2021-07-14 15:28:35 +0300

    Fixed with null data response

--- src/store/user/actions/index.ts
+++
@@ -1,6 +0,0 @@
-export { changeUserId } from './changeUserId';
-export {
-  getUserDataStart,
-  getUserDataSuccess,
-  getUserDataFailure,
-} from './getUserData';
--- src/store/user/sagas/index.ts
+++
@@ -1,6 +0,0 @@
-import { call, all } from 'redux-saga/effects';
-import { getUserDataSagas } from './getUserAsync';
-
-export function* userDataSagas() {
-  yield all([call(getUserDataSagas)]);
-}
