eeb7d58d28a9ff266639b6aaeb9b08018496c523
Alexander <gituser1056@gmail.com>
2021-07-29 20:24:06 +0300

    OAuth 100%

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import s from './home.module.scss';
-import {StartGameBanner} from "@components/StartGameBanner/start-game-banner";
+import { StartGameBanner } from '@components/StartGameBanner/start-game-banner';
+import * as api from '../../services/api';
+import { getAuthCodeFromQuery } from '../../utlis/get-auth-code-from-query';
 
 export const Home = (): JSX.Element => {
+  useEffect(() => {
+    const authCode = getAuthCodeFromQuery();
+
+    if (authCode)
+      api
+        .OAuthLogin({
+          code: authCode,
+          redirect_uri: 'https://localhost:5000',
+        })
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+  }, []);
+
   return (
-      <div className={s.homePage}>
-          <StartGameBanner />
-      </div>
+    <div className={s.homePage}>
+      <StartGameBanner />
+    </div>
   );
 };
---
+++ webpack.dev.config.ts
@@ -0,0 +1,104 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import * as webpack from 'webpack';
+import 'webpack-dev-server';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  output: {
+    publicPath: '/',
+  },
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]___[hash:base64:5]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.tsx', '.ts', '.js', '.scss'],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      MODE: 'development',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/favicon.ico', to: '' },
+        { from: './src/manifest.json', to: '' },
+        { from: './src/logo192.png', to: '' },
+        { from: './src/logo512.png', to: '' },
+      ],
+    }),
+    new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: './src/src-sw',
+      swDest: 'sw.js',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+    }),
+  ],
+  devtool: 'inline-source-map',
+  // @ts-ignore
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    historyApiFallback: true,
+    port: 5000,
+    open: true,
+    hot: true,
+  },
+};
+
+export default config;
