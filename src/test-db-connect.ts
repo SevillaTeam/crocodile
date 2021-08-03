4a0b0da3b8af3d9fc9de34b2763855013703267f
Alexander <gituser1056@gmail.com>
2021-08-03 15:35:29 +0300

    docker-compose поправил

---
+++ src/test-db-connect.ts
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+
+export const testDBConnect = () => {
+  client.connect();
+
+  // @ts-ignore
+  client
+    .query('SELECT NOW()')
+    // @ts-ignore
+    .then((res) => {
+      console.log('Тестовый ответ от DB на запрос SELECT NOW()=', res.rows);
+      client.end();
+      // @ts-ignore
+    })
+    // @ts-ignore
+    .catch((err) => {
+      console.log('error', err);
+    });
+};
