d9117ce31d31d3df2056b52149ad1e13c320af1f
Alexander <gituser1056@gmail.com>
2021-08-05 20:13:23 +0300

    Добавил в README описание для контейнеров

--- src/components/TestThemes/redux-saga/actions/getUserData.ts
+++ src/components/TestThemes/redux-saga/actions/getUserData.ts
@@ -4,10 +4,6 @@ import { IResponseUserState } from '../interfaces';
 export const getUserDataStart = () => {
   return {
     type: t.GET_USER_DATA_START,
-<<<<<<< HEAD
-=======
-    payload: {},
->>>>>>> 20b363b (Добавил в README описание для контейнеров)
   };
 };
 
